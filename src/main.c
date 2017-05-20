218dd0a4956e9cafe8559f8b061197e9ea2c0eee
Gabriel Imre <gimre@endava.net>
2017-05-20 23:43:38 +0300

    added rclick to quit

--- src/main.c
+++ src/main.c
@@ -2,9 +2,51 @@
 #include "utils.h"
 
 #define WIN32_LEAN_AND_MEAN
+#define WM_USER_SHELLICON ( WM_USER + 1 )
 
-const char WINDOW_CLASS[ ]   = "Trey manager";
-const UINT WM_USER_SHELLICON = WM_USER + 1;
+
+const char WINDOW_CLASS[ ] = "Trey manager";
+
+void createShellIcon( HWND hwnd ) {
+    NOTIFYICONDATA iconData = createShellIconData( hwnd, WM_USER_SHELLICON );
+    Shell_NotifyIcon( NIM_ADD, &iconData );
+}
+
+void handleTrayIconMessage(
+    HWND   hwnd,
+    UINT   message,
+    WPARAM wParam,
+    LPARAM lParam
+) {
+    switch( lParam ) {
+        case WM_LBUTTONUP:
+            MessageBox( hwnd, "You clicked?", "Yes", MB_OK );
+            break;
+        case WM_RBUTTONUP:
+            PostQuitMessage( 0 );
+            break;
+        default:
+            break;
+    }
+}
+
+void handleWindowMessage(
+    HWND   hwnd,
+    UINT   message,
+    WPARAM wParam,
+    LPARAM lParam
+) {
+    switch( message ) {
+        case WM_CREATE:
+            createShellIcon( hwnd );
+            break;
+        case WM_USER_SHELLICON:
+            handleTrayIconMessage( hwnd, message, lParam, wParam );
+            break;
+        default:
+            break;
+    }
+}
 
 LRESULT CALLBACK WindowProcedure(
     HWND   hwnd,
@@ -12,6 +54,7 @@ LRESULT CALLBACK WindowProcedure(
     WPARAM wParam,
     LPARAM lParam
 ) {
+    handleWindowMessage( hwnd, message, wParam, lParam );
     return DefWindowProc( hwnd, message, wParam, lParam );
 }
 
@@ -22,17 +65,13 @@ int WINAPI WinMain (
     int       nCmdShow
 ) {
     MSG  messages;
-    HWND hwnd;
 
     WNDCLASSEX windowClass = createWindowClass( WINDOW_CLASS, WindowProcedure );
     if( ! RegisterClassEx( &windowClass ) ) {
         return 0;
     }
 
-    hwnd = createMessageOnlyWindow( WINDOW_CLASS );
-
-    NOTIFYICONDATA iconData = createShellIconData( hwnd, WM_USER_SHELLICON );
-    Shell_NotifyIcon( NIM_ADD, &iconData );
+    createMessageOnlyWindow( WINDOW_CLASS );
 
     while( GetMessage( &messages, NULL, 0, 0 ) ) {
         TranslateMessage( &messages );
